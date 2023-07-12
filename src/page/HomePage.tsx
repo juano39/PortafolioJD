788daa4d0b8b0f5b0a2b1f076a4360cfb72a672f
juan.valderrama <juan.valderrama@arus.com.co>
2023-07-12 10:00:26 -0500

    cambio tamaño texto del homePage

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -12,7 +12,7 @@ export const HomePage = () => {
     <header className="mb-[150px] ">
       <h3 data-aos="fade" className="text-[32px] text-[#E0E0E0]">i`m a</h3>
 
-      <h1 data-aos="fade-right" className="max-w-[480px] min-[370px]:text-[96px] max-[370px]:text-[70px]  leading-none mb-[50px]">
+      <h1 data-aos="fade-right" className="max-w-[480px] md:text-[96px] text-[70px]  leading-none mb-[50px]">
         Full Stack SoftWare Developer
         <span className="ml-1 text-[#1C9EE7] scale-75 translate-x-4 skew-y-3 text-2xl">
           ▖

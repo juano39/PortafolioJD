c93d8d8f119d89a432e9637e3fc4a57077625c02
juan.valderrama <juan.valderrama@arus.com.co>
2023-07-12 22:51:03 -0500

    kyframe

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -10,15 +10,16 @@ export const HomePage = () => {
   }, []);
   return (
     <header id="home" className="mb-[150px] ">
-      <section className="flex flex-wrap lg:justify-between gap-6 max-lg:justify-center items-center ">
-        <article className="max-lg:w-full flex flex-col max-lg:items-center">
-          <h3 data-aos="fade" className="text-[32px] text-[#E0E0E0]">
+      <section className="flex relative flex-wrap justify-between gap-6 max-lg:justify-center items-center ">
+        <article className="max-lg:w-full items-start flex flex-col ">
+          <h3 data-aos="fade" className="text-[32px] max-sm:text-2xl text-[#E0E0E0]">
+
             Juan Daniel Valderrama
           </h3>
 
           <h1
             data-aos="fade-right"
-            className="max-w-[480px] font-bold md:text-[105px] text-5xl  sm:text-[70px] leading-none mb-[50px]"
+            className="max-w-[480px] font-bold md:text-[105px] text-6xl  sm:text-[70px] leading-none mb-[50px]"
           >
             Front End Developer
             <span className="ml-1 text-[#1C9EE7] scale-75 translate-x-4 skew-y-3 text-2xl">
@@ -27,7 +28,10 @@ export const HomePage = () => {
           </h1>
           <CustomButton text="Previous Projects" />
         </article>
-        <img className="w-[40%]" src="developer.png" alt="" />
+        
+          
+        <img className="sm:w-[40%] w-[80%] relative top-1/2  animate-flotin" src="developer.png" alt="" />
+        
       </section>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flotin: {
+          "0%, 100%": { transform: "translateY(-2%)" },
+
+          "50% ": { transform: "translateY(-0%)" },
+
+          
+        },
+      },
+      animation: {
+        flotin: 'flotin 4s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
-}
-
+};
